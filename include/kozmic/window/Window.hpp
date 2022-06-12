1c0739817ac317655473c965bcc584bbb94a395e
Vitor Chaves <vitorclcoelho@protonmail.com>
2022-06-12 19:12:14 -0300

    Add fullscreen mode configs to the window interface

--- include/kozmic/window/Window.hpp
+++ include/kozmic/window/Window.hpp
@@ -14,14 +14,21 @@ namespace Kozmic::Core::Window {
 		unsigned int yPos;
 	};
 
+	enum KWindowMode {
+		WINDOWED,
+		FULLSCREEN,
+		EXCLUSIVE_FULLSCREEN
+	};
+
 	class KWindow {
 	protected:
 		std::string m_sTitle;
 		KWindowSize m_size;
 		KWindowPosition m_position;
+		KWindowMode m_mode;
 
 	public:
-		KWindow(std::string t_sTitle, KWindowSize t_size, KWindowPosition t_position);
+		KWindow(std::string t_sTitle, KWindowSize t_size, KWindowPosition t_position, KWindowMode t_mode);
 
 		virtual void show() = 0;
 		virtual void hide() = 0;
@@ -33,10 +40,12 @@ namespace Kozmic::Core::Window {
 		virtual void setTitle(std::string t_sTitle) = 0;
 		virtual void setSize(KWindowSize t_size) = 0;
 		virtual void setPosition(KWindowPosition t_position) = 0;
+		virtual void setMode(KWindowMode t_mode) = 0;
 
 	public:
 		std::string getTitle();
 		KWindowSize getSize();
 		KWindowPosition getPosition();
+		KWindowMode getMode();
 	};
 }
--- lib/window/Window.cpp
+++ lib/window/Window.cpp
@@ -2,7 +2,7 @@
 
 using namespace Kozmic::Core::Window;
 
-KWindow::KWindow(std::string t_sTitle, KWindowSize t_size, KWindowPosition t_position) {
+KWindow::KWindow(std::string t_sTitle, KWindowSize t_size, KWindowPosition t_position, KWindowMode t_mode) {
 	this->m_sTitle = t_sTitle;
 	this->m_size = t_size;
 	this->m_position = t_position;
@@ -21,4 +21,8 @@ KWindowSize KWindow::getSize()
 KWindowPosition KWindow::getPosition()
 {
 	return this->m_position;
+}
+
+KWindowMode KWindow::getMode() {
+	return this->m_mode;
 }
