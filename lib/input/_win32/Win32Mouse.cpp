bb2337eececbb9f665243441334923edbe43dd0d
Vitor Chaves <vitorclcoelho@protonmail.com>
2022-06-15 19:25:55 -0300

    Add a win32 mouse input implementation

---
+++ lib/input/_win32/Win32Mouse.cpp
@@ -0,0 +1,15 @@
+#include "Win32Mouse.hpp"
+
+using namespace Kozmic::Core::Input::Win32;
+
+void K_Win32Mouse::notifyMove(unsigned int t_nXPos, unsigned int t_nYPos)
+{
+}
+
+void K_Win32Mouse::notifyButtonUp(WPARAM t_wButton)
+{
+}
+
+void K_Win32Mouse::notifyButtonDown(WPARAM t_wButton)
+{
+}
---
+++ lib/input/_win32/Win32Mouse.hpp
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include <Windows.h>
+
+#include <kozmic/input/Mouse.hpp>
+
+namespace Kozmic::Core::Window::Win32 {
+	class KWin32Window;
+}
+
+namespace Kozmic::Core::Input::Win32 {
+	class K_Win32Mouse : public K_Mouse {
+	private:
+		friend class Window::Win32::KWin32Window;
+
+	private:
+		void notifyMove(unsigned int t_nXPos, unsigned int t_nYPos);
+		void notifyButtonUp(WPARAM t_wButton);
+		void notifyButtonDown(WPARAM t_wButton);
+	};
+}
